1fbab6a60e3592928932cd4605bbd7b4860621f8
jungwoon <jungwoon018@gmail.com>
2021-05-17 00:55:05 +0900

    3.5 템플릿/콜백 시작 전

---
+++ ch3_템플릿/05_template_and_callback/src/dao/JdbcContext.java
@@ -0,0 +1,37 @@
+package dao;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// UserDao 에 있던 jdbcContextWithStatementStrategy()를 다른 DAO 클래스에서 사용할 수 있게 독립시키기 위한 클래스
+// DataSource 와는 달리 구체 클래스다.
+// 독립적인 JDBC 컨텍스트를 제공해주는 서비스 오브젝트고 구현 방법이 바뀔 가능성이 없어서
+// 따로 인터페이스를 구현하도록 만들지 않았다.
+public class JdbcContext {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    // 메서드 사용시 전략만 주입하면 어디서든지 편하게 사용할 수 있다.
+    public void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = this.dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw e;
+        }finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e ) { }}
+            if (c != null) { try { c.close(); } catch (SQLException e ) { }}
+        }
+    }
+}
---
+++ ch3_템플릿/05_template_and_callback/src/dao/UserDao.java
@@ -0,0 +1,103 @@
+package dao;
+
+
+import domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private DataSource dataSource; // db 연결 클래스
+    private JdbcContext jdbcContext; // 쿼리 전략 클래스
+
+    // 수정자 메소드이면서 JdbcContext 에 대한 생성, DI 작업을 동시에 수행한다.
+    public void setDataSource(DataSource dataSource) {
+        // JdbcContext 생성 (IoC)
+        this.jdbcContext = new JdbcContext();
+        // 의존 오브젝트 주임(DI)
+        this.jdbcContext.setDataSource(dataSource);
+
+        this.dataSource = dataSource;
+    }
+
+    public void add(final User user) throws SQLException {
+
+
+        this.jdbcContext.workWithStatementStrategy(
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection con) throws SQLException {
+                        // 쿼리 준비
+                        PreparedStatement ps = con.prepareStatement("insert into user (id, name, password) value (?,?,?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
+
+                        return ps;
+                    }
+                }
+        );
+    }
+
+    public User get(String id) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+
+        // 쿼리 준비
+        PreparedStatement ps = con.prepareStatement("select * from user where id = ?");
+        ps.setString(1, id);
+
+        // executeQuery() 는 쿼리를 실행한 결과 set 을 return 한다.
+        ResultSet rs = ps.executeQuery();
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
+        // 연결 끊기
+        rs.close();
+        ps.close();
+        con.close();
+
+        if (user == null)
+            throw new EmptyResultDataAccessException(1);
+
+        return user;
+    }
+
+    public void deleteAll() throws SQLException {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps;
+                ps = c.prepareStatement("delete from user");
+
+                return ps;
+            }
+        });
+    }
+
+    public int getCount() throws SQLException {
+        Connection con = dataSource.getConnection();
+
+        PreparedStatement ps = con.prepareStatement("select count(*) from user");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        con.close();
+
+        return count;
+    }
+}
