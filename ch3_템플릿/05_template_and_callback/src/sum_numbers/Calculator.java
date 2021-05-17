152e2e5530b4bd35c82fe6551a06c44ae4737742
jungwoon <jungwoon018@gmail.com>
2021-05-17 14:21:00 +0900

    3.5.3 템플릿/콜백의 응용 - 중복의 제거와 템플릿/콜백 설계

--- ch3_템플릿/05_template_and_callback/src/sum_numbers/Calculator.java
+++ ch3_템플릿/05_template_and_callback/src/sum_numbers/Calculator.java
@@ -6,19 +6,46 @@ import java.io.IOException;
 import java.net.URLDecoder;
 
 public class Calculator {
+    // 템플릿 콜백을 적용한 calcSum() 메소드
     public Integer calcSum(String filepath) throws IOException {
+         return fileReadTemplate(filepath, new BufferedReaderCallback() {
+             @Override
+             public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                 Integer sum = 0;
+                 String line = null;
+                 while ((line = br.readLine()) !=null) {
+                     sum += Integer.valueOf(line);
+                 }
+                 return sum;
+             }
+         });
+    }
+
+    // 곱을 계산하는 콜백을 가진 calcMultiply() 메소드
+    public Integer calcMultiply(String filepath) throws IOException {
+        return fileReadTemplate(filepath, new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer multiply = 1;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    multiply *= Integer.valueOf(line);
+                }
+                return multiply;
+            }
+        });
+    }
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
         String path = URLDecoder.decode(filepath, "UTF-8"); // 한글로된 파일명 때문에 오류가 생겨 UTF-8로 디코딩
 
         BufferedReader br = null;
 
         try {
             br = new BufferedReader(new FileReader(path)); // 한 줄씩 읽기 편하게  BufferedReader 로 가져온다.
-            Integer sum = 0;
-            String line = null;
-            while ((line = br.readLine()) != null) { // 마지막 라인까지 한 줄씩 읽어가면서 숫자를 더한다.
-                sum += Integer.valueOf(line);
-            }
-            return sum;
+            // 콜백 오브젝트 호출. 템플릿에서 만든 컨텍스트 정보인 BufferedReader 를 전달해주고 콜백의 작업 결과를 받아둔다.
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
